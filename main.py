d07efcd6d8e3d338c5945e1a3a0adb7eb3029a95
Evan Ostroski <eostroski@gmail.com>
2023-11-13 00:55:27 +0000

    SequentialChain

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 from dotenv import load_dotenv
+import argparse
 
 load_dotenv()
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--task", default="return a list of numbers")
+parser.add_argument("--language", default="python")
+
+args = parser.parse_args()
 
 llm = OpenAI()
 
@@ -12,13 +19,32 @@ code_prompt = PromptTemplate(
     input_variables=["language", "task"]
 )
 
-code_chain = LLMChain(
-    llm=llm,
-    prompt=code_prompt
+test_prompt = PromptTemplate(
+    template="Write a test for the following {language} code:\n{code}",
+    input_variables=["language","code"]
 )
 
-result = code_chain({
-    "language":"python",
-    "task":"returns a list of numbers"})
+code_chain = LLMChain(
+    llm=llm,
+    prompt=code_prompt,
+    output_key="code"
+)
+
+test_chain = LLMChain(
+    llm=llm,
+    prompt=test_prompt,
+    output_key="test"
+)
+
+chain = SequentialChain(
+    chains=[code_chain, test_chain],
+    input_variables=["language", "task"],
+    output_variables=["code", "test"]
+)
+
+result = chain({
+    "language":args.language,
+    "task":args.task
+})
 
 print(result)
