60805ea054a37b5baa4034944e9701633632073f
Evan Ostroski <eostroski@gmail.com>
2023-11-19 19:01:22 +0000

    ChromaDB with OpenAI embeddings

--- main.py
+++ main.py
@@ -2,17 +2,15 @@
 from langchain.document_loaders import TextLoader
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores.chroma import Chroma
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 embeddings = OpenAIEmbeddings()
 
-emb = embeddings.embed_query("What does the fox say?")
-
-print(emb)
-
-""" text_splitter = CharacterTextSplitter (
+text_splitter = CharacterTextSplitter (
     separator="\n",
     chunk_size=200,
     chunk_overlap=0
@@ -23,6 +21,18 @@ docs = loader.load_and_split(
     text_splitter=text_splitter
 )
 
-for doc in docs:
-    print(doc.page_content)
-    print("\n") """
+db = Chroma.from_documents(
+    docs,
+    embedding=embeddings,
+    persist_directory="emb"
+)
+
+results = db.similarity_search_with_score(
+    "What is an interesting fact about the English language?",
+    k=2
+)
+
+for result in results:
+    print("\n")
+    print(result[1])
+    print(result[0].page_content)
