773ca8da235029e88bfdaca80b893961613fff26
Evan Ostroski <eostroski@gmail.com>
2023-11-20 20:28:55 +0000

    custom embeddings retriever

--- prompt.py
+++ prompt.py
@@ -2,7 +2,11 @@ from langchain.vectorstores.chroma import Chroma
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
+from redundant_filter_retriever import RedundantFilterRetriever
 from dotenv import load_dotenv
+import langchain
+
+langchain.debug = True
 
 load_dotenv()
 
@@ -12,6 +16,11 @@ db = Chroma(
     persist_directory="emb",
     embedding_function=embeddings
 )
+# retriever = RedundantFilterRetriever(
+#     embeddings=embeddings,
+#     chroma=db
+# )
+
 retriever = db.as_retriever()
 
 chain = RetrievalQA.from_chain_type(
