8a21fd67de4552000d6f3574223ab20961c2ad73
Evan Ostroski <eostroski@gmail.com>
2023-12-08 19:46:40 +0000

    Streamable :bug:

---
+++ pdf-dist/app/chat/chains/streamable.py
@@ -0,0 +1,24 @@
+from flask import current_app
+from queue import Queue
+from threading import Thread
+from app.chat.callbacks.stream import StreamingHandler
+
+class StreamableChain:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def stream(self, input):
+        queue = Queue()
+        handler = StreamingHandler(queue=queue)
+
+        def task(app_context):
+            app_context.push()
+            self(input, callbacks=[handler])
+
+        Thread(target=task, args=[current_app.app_context()]).start()
+
+        while True:
+            token = queue.get()
+            if token is None:
+                break
+            yield token
