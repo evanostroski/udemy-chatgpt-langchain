30023a677357448f4867ab239af6b8b93f0c2fc9
Evan Ostroski <show>
2023-12-15 18:50:45 +0000

    add component maps

--- pdf-dist/app/chat/vector_stores/pinecone.py
+++ pdf-dist/app/chat/vector_stores/pinecone.py
@@ -12,9 +12,10 @@ vector_store = Pinecone.from_existing_index(
     os.getenv("PINECONE_INDEX_NAME"), embeddings
 )
 
-def build_retriever(chat_args):
+def build_retriever(chat_args, k):
     search_kwargs = {
-        "filter": { "pdf_id": chat_args.pdf_id }
+        "filter": { "pdf_id": chat_args.pdf_id },
+        "k": k
     }
     return vector_store.as_retriever(
         search_kwargs=search_kwargs
